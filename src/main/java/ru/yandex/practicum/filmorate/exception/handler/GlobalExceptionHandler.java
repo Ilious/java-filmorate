ae0e8a9f2204c618f1b08d5a76de0f934e433f8b
I1nur <tuxtarov0475@gmail.com>
2025-04-12 20:13:59 +0400

    fix: add handling annotation validation exception

--- src/main/java/ru/yandex/practicum/filmorate/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.exception.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.EntityExistsException;
@@ -23,6 +24,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleAlreadyExistsException(EntityExistsException exception) {
         log.warn("handleAlreadyExistsException {}", exception.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleValidationAnnotationException(MethodArgumentNotValidException exception) {
+        log.warn("handleValidationAnnotationException {}", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 
     @ExceptionHandler
