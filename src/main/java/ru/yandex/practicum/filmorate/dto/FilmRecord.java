08da7a592697cf0cd2ff910dbaeab2e4f0fe080f
I1nur <tuxtarov0475@gmail.com>
2025-05-12 09:17:23 +0400

    feature: record validations now with validated by

--- src/main/java/ru/yandex/practicum/filmorate/dto/FilmRecord.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/FilmRecord.java
@@ -2,19 +2,30 @@ package ru.yandex.practicum.filmorate.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import ru.yandex.practicum.filmorate.validator.DateBefore;
+import ru.yandex.practicum.filmorate.validator.Validator;
 
 import java.time.LocalDate;
+import java.util.List;
 
-public record FilmRecord(Long id,
+public record FilmRecord(
+        @NotNull(groups = Validator.OnUpdate.class) Long id,
 
-                         @NotBlank String name,
+        @NotBlank(groups = {Validator.OnUpdate.class, Validator.OnCreate.class}) String name,
 
-                         @NotBlank @Size(max = 200) String description,
+        @NotBlank(groups = Validator.OnCreate.class)
+        @Size(max = 200, groups = {Validator.OnUpdate.class, Validator.OnCreate.class}) String description,
 
-                         @DateBefore @JsonFormat(pattern = "yyyy-MM-dd") LocalDate releaseDate,
+        MpaRecord mpa,
 
-                         @Positive int duration) {
+        @DateBefore(groups = {Validator.OnUpdate.class, Validator.OnCreate.class})
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate releaseDate,
+
+        @Positive(groups = {Validator.OnUpdate.class, Validator.OnCreate.class})
+        int duration,
+
+        List<GenreRecord> genres) {
 }
