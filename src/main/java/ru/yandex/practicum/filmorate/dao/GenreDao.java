27c574f2582e9f73129893d8c506c74e01ac7a44
I1nur <tuxtarov0475@gmail.com>
2025-05-12 09:16:43 +0400

    fix: daos

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/GenreDao.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.yandex.practicum.filmorate.dao.enums.Genre;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenreDao {
+
+    private Long id;
+
+    private Genre name;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/MpaDao.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.yandex.practicum.filmorate.dao.enums.AgeRating;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MpaDao {
+
+    private Long id;
+
+    private AgeRating name;
+}
