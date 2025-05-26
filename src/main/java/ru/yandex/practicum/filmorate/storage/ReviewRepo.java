5bc8afe9c59ad96aeabda4d41359181782222a19
NadezhdaTA <nadezda.tupitsyna@yandex.ru>
2025-05-26 16:07:08 +0400

    Добавлена функциональность "Отзывы"

--- src/main/java/ru/yandex/practicum/filmorate/storage/ReviewRepo.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewRepo.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @Slf4j
 @Component
-public class ReviewRepo extends BaseRepo<ReviewDao> implements IReviewRepo{
+public class ReviewRepo extends BaseRepo<ReviewDao> implements IReviewRepo {
 
     public ReviewRepo(JdbcTemplate jdbc, RowMapper<ReviewDao> mapper) {
         super(jdbc, mapper);
