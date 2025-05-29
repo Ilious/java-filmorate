af3cf434cd0c2eca5b8fe850360fc77ac5a1d502
I1nur <tuxtarov0475@gmail.com>
2025-05-30 00:04:26 +0400

    checkstyle: unused imports

--- src/main/java/ru/yandex/practicum/filmorate/storage/mapper/DirectorMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mapper/DirectorMapper.java
@@ -9,7 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Component
 public class DirectorMapper implements RowMapper<DirectorDao> {
